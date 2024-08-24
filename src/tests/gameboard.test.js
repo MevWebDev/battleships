8c164cab31392ea9f31505aee6cfb6a792ce14df
MevWebDev <szymongrysiewicz@gmail.com>
2024-08-25 00:01:47 +0200

    more tests! (I wont add specifis tests cuz it would be too long and I am lazy"

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -33,10 +33,35 @@ test("updates hits value if ship was hit", () => {
   expect(ship.hits).toBe(1);
 });
 
+test("update is sunk if ship was sunk", () => {
+  const gameboard = new Gameboard();
+  const ship = new Ship(1);
+  gameboard.placeShip(ship, 0, 0, "vertically");
+  gameboard.receiveAttack(0, 0);
+  expect(ship.isSunk()).toBe(true);
+});
+
 test("places 'miss' on empty field", () => {
   const gameboard = new Gameboard();
   const ship = new Ship(4);
   gameboard.placeShip(ship, 0, 0, "vertically");
   gameboard.receiveAttack(5, 5);
   expect(gameboard.board[5][5]).toBe("miss");
+});
+
+test("checks if all ships are not sunk", () => {
+  const gameboard = new Gameboard();
+  const ship = new Ship(1);
+  gameboard.ships.push(ship);
+  gameboard.placeShip(ship, 0, 0, "vertically");
+  expect(gameboard.areAllShipsSunk()).toBe(false);
+});
+
+test("checks if all ships are sunk after hitting last ship", () => {
+  const gameboard = new Gameboard();
+  const ship = new Ship(1);
+  gameboard.ships.push(ship);
+  gameboard.placeShip(ship, 0, 0, "vertically");
+  gameboard.receiveAttack(0, 0);
+  expect(gameboard.areAllShipsSunk()).toBe(true);
 });
