fd355d6328437d02b99331d9133d888227f3772c
MevWebDev <szymongrysiewicz@gmail.com>
2024-08-25 00:02:07 +0200

    no ships for better tests

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,8 +1,10 @@
+import Ship from "./ship";
 export default class Gameboard {
   constructor() {
     this.board = Array(10)
       .fill(null)
       .map(() => Array(10).fill(null));
+    this.ships = [];
   }
   placeShip(ship, x, y, direction) {
     if (direction === "horizontally") {
@@ -21,11 +23,18 @@ export default class Gameboard {
   }
   receiveAttack(x, y) {
     if (typeof this.board[x][y] === "object" && this.board[x][y] !== null) {
-      this.board[x][y].hits += 1;
+      this.board[x][y].hit();
       return true;
     } else {
       this.board[x][y] = "miss";
       return false;
     }
+  }
+  areAllShipsSunk() {
+    const result = this.ships.filter((ship) => {
+      return ship.isSunk() === false;
+    });
+    if (result.length > 0) return false;
+    else return true;
   }
 }
