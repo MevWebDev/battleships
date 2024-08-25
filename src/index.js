44938ce773b658a14358974794f62bd57e35c2a3
MevWebDev <szymongrysiewicz@gmail.com>
2024-08-25 20:09:14 +0200

    index js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import "./style.scss";
-import Ship from "./ship";
-import Gameboard from "./gameboard";
+import Ship from "./logic/ship";
+import Gameboard from "./logic/gameboard";
+import Player from "./logic/Player";
+import Computer from "./logic/computer";
+import appUI from "./components/app";
+import playerPutsShips from "./logic/playerPutsShips";
+import computerPutsShips from "./logic/computerPutsShips";
+
+const player = new Player();
+
+const computer = new Computer();
+
+appUI();
+playerPutsShips(player);
+computerPutsShips(computer);
