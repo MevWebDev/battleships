118f86caaa27b7b787cd95e834aeec10f84aa7aa
MevWebDev <szymongrysiewicz@gmail.com>
2024-08-25 20:08:59 +0200

    board component

---
+++ src/components/boardUI.js
@@ -0,0 +1,14 @@
+export default function boardUI() {
+  const board = document.createElement("div");
+  board.classList.add("board");
+  board.classList.add("game-board");
+  for (let i = 0; i < 10; i += 1) {
+    for (let j = 0; j < 10; j += 1) {
+      const field = document.createElement("div");
+      field.classList.add(`field-${i}-${j}`);
+      field.classList.add("field");
+      board.appendChild(field);
+    }
+  }
+  return board;
+}
