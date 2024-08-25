35e84929d95916fe6672a8598bb3edb60c5396d6
MevWebDev <szymongrysiewicz@gmail.com>
2024-08-25 12:53:39 +0200

    <feat> Added 5 ships in gameboard initialization

--- src/gameboard.js
+++ src/gameboard.js
@@ -4,37 +4,42 @@ export default class Gameboard {
     this.board = Array(10)
       .fill(null)
       .map(() => Array(10).fill(null));
-    this.ships = [];
+    this.ships = [
+      new Ship(5),
+      new Ship(4),
+      new Ship(3),
+      new Ship(3),
+      new Ship(2),
+    ];
   }
+
   placeShip(ship, x, y, direction) {
     if (direction === "horizontally") {
       if (x + ship.length <= 9) {
-        for (let i = 0; i < ship.length; i++) {
+        for (let i = 0; i < ship.length; i += 1) {
           this.board[x][y + i] = ship;
         }
       }
     } else if (direction === "vertically") {
       if (y + ship.length <= 9) {
-        for (let i = 0; i < ship.length; i++) {
+        for (let i = 0; i < ship.length; i += 1) {
           this.board[x + i][y] = ship;
         }
       }
     }
   }
+
   receiveAttack(x, y) {
     if (typeof this.board[x][y] === "object" && this.board[x][y] !== null) {
       this.board[x][y].hit();
       return true;
-    } else {
-      this.board[x][y] = "miss";
-      return false;
     }
+    this.board[x][y] = "miss";
+    return false;
   }
+
   areAllShipsSunk() {
-    const result = this.ships.filter((ship) => {
-      return ship.isSunk() === false;
-    });
-    if (result.length > 0) return false;
-    else return true;
+    const result = this.ships.filter((ship) => !ship.isSunk());
+    return result.length === 0;
   }
 }
