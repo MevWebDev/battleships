c7cf3f171b466d6bc8bc62c67b838db2fe49a946
MevWebDev <szymongrysiewicz@gmail.com>
2024-08-29 23:16:45 +0200

    <fix> changed named directions to x/y axis

--- src/logic/playerPutsShips.js
+++ src/logic/playerPutsShips.js
@@ -6,10 +6,14 @@ let currentMouseOutFunction = null;
 const hoverColor = "rgb(173, 216, 230)";
 
 export default function playerPutsShips(player) {
-  const gameboard = document.querySelector(".board");
+  const popup = document.querySelector(".popup");
+  console.log(popup);
   let index = 0;
-  const board = document.querySelector(".game-board");
+  const board = document.querySelector(".popup-board");
+  console.log(board);
+
   const fields = board.childNodes;
+
   const playerBoard = player.gameboard;
   const playerShips = playerBoard.ships;
 
@@ -22,36 +26,37 @@ export default function playerPutsShips(player) {
       const x = parseInt(parts[1], 10);
       const y = parseInt(parts[2].split(" ")[0], 10);
       const ship = playerShips[index];
+
       if (playerBoard.placeShip(ship, x, y, player.direction)) {
-        if (index === 5) {
-          removeHover();
-          return;
-        }
         index += 1;
         removeHover();
         hoverPutting(playerBoard, player, index);
       }
+      if (index === 5) {
+        popup.style.display = "none";
+        return;
+      }
 
-      displayShips(player, gameboard);
+      displayShips(player, board);
     });
   });
 }
 
 function handleMouseOver(gameboard, player, index) {
+  const board = document.querySelector(".popup-board");
   return function () {
     console.log(index);
     const x = parseInt(this.className.split("-")[1]);
     const y = parseInt(this.className.split("-")[2].split(" ")[0]);
 
-    if (
-      gameboard.board[x][y] !== null ||
-      y + gameboard.ships[index].length > 10
-    ) {
-      this.style.cursor = "not-allowed";
-      return;
-    }
-
-    if (player.direction === "horizontally") {
+    if (player.direction === "x") {
+      if (
+        gameboard.board[x][y] !== null ||
+        y + gameboard.ships[index].length > 10
+      ) {
+        this.style.cursor = "not-allowed";
+        return;
+      }
       if (y + gameboard.ships[index].length <= 10) {
         // checkin if u can place ship
         for (let i = 0; i < gameboard.ships[index].length; i += 1) {
@@ -67,7 +72,39 @@ function handleMouseOver(gameboard, player, index) {
             return;
           }
 
-          const field = document.querySelector(`.field-${x}-${y + i}`);
+          const field = board.querySelector(`.field-${x}-${y + i}`);
+
+          if (field !== null) {
+            if (field.style.backgroundColor === "transparent")
+              field.style.backgroundColor = hoverColor;
+          }
+        }
+      }
+    }
+    if (player.direction === "y") {
+      if (
+        gameboard.board[x][y] !== null ||
+        x + gameboard.ships[index].length > 10
+      ) {
+        this.style.cursor = "not-allowed";
+        return;
+      }
+      if (x + gameboard.ships[index].length <= 10) {
+        // checkin if u can place ship
+        for (let i = 0; i < gameboard.ships[index].length; i += 1) {
+          if (gameboard.board[x + i][y] !== null) {
+            this.style.cursor = "not-allowed";
+            return;
+          }
+        }
+        // hovering over fields
+        for (let i = 0; i < gameboard.ships[index].length; i += 1) {
+          if (gameboard.board[x + i][y] !== null) {
+            this.style.cursor = "not-allowed";
+            return;
+          }
+
+          const field = board.querySelector(`.field-${x + i}-${y}`);
 
           if (field !== null) {
             if (field.style.backgroundColor === "transparent")
@@ -79,16 +116,29 @@ function handleMouseOver(gameboard, player, index) {
   };
 }
 function handleMouseOut(gameboard, player, index) {
+  const board = document.querySelector(".popup-board");
   return function () {
     const x = parseInt(this.className.split("-")[1]);
     const y = parseInt(this.className.split("-")[2].split(" ")[0]);
 
     if (this.style.backgroundColor !== hoverColor) return;
 
-    if (player.direction === "horizontally") {
+    if (player.direction === "x") {
       if (y + gameboard.ships[index].length <= 10) {
         for (let i = 0; i < gameboard.ships[index].length; i += 1) {
-          const field = document.querySelector(`.field-${x}-${y + i}`);
+          const field = board.querySelector(`.field-${x}-${y + i}`);
+
+          if (field !== null) {
+            if (field.style.backgroundColor === hoverColor)
+              field.style.backgroundColor = "transparent";
+          }
+        }
+      }
+    }
+    if (player.direction === "y") {
+      if (x + gameboard.ships[index].length <= 10) {
+        for (let i = 0; i < gameboard.ships[index].length; i += 1) {
+          const field = board.querySelector(`.field-${x + i}-${y}`);
 
           if (field !== null) {
             if (field.style.backgroundColor === hoverColor)
@@ -102,8 +152,9 @@ function handleMouseOut(gameboard, player, index) {
 
 function hoverPutting(gameboard, player, index) {
   if (index === 5) return;
-  const board = document.querySelector(".game-board");
+  const board = document.querySelector(".popup-board");
   const fields = board.childNodes;
+
   currentMouseOverFunction = handleMouseOver(gameboard, player, index);
   currentMouseOutFunction = handleMouseOut(gameboard, player, index);
   fields.forEach((field) => {
@@ -113,7 +164,7 @@ function hoverPutting(gameboard, player, index) {
 }
 
 function removeHover() {
-  const board = document.querySelector(".game-board");
+  const board = document.querySelector(".popup-board");
   const fields = board.childNodes;
   if (currentMouseOverFunction && currentMouseOutFunction) {
     fields.forEach((field) => {
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,18 +1,18 @@
 import Gameboard from "../logic/gameboard";
 import Ship from "../logic/ship";
 
-test("places ship correctly horizontally", () => {
+test("places ship correctly x", () => {
   const gameboard = new Gameboard();
   const ship = new Ship(4);
-  gameboard.placeShip(ship, 0, 0, "horizontally");
+  gameboard.placeShip(ship, 0, 0, "x");
   for (let i = 0; i < ship.length; i++) {
     expect(gameboard.board[0][0 + i]).toBe(ship);
   }
 });
-test("places ship correctly vertically", () => {
+test("places ship correctly y", () => {
   const gameboard = new Gameboard();
   const ship = new Ship(4);
-  gameboard.placeShip(ship, 0, 0, "vertically");
+  gameboard.placeShip(ship, 0, 0, "y");
   for (let i = 0; i < ship.length; i++) {
     expect(gameboard.board[0 + i][0]).toBe(ship);
   }
@@ -21,7 +21,7 @@ test("places ship correctly vertically", () => {
 test("attack simulation - check coordinates and reports git/not hit", () => {
   const gameboard = new Gameboard();
   const ship = new Ship(4);
-  gameboard.placeShip(ship, 0, 0, "vertically");
+  gameboard.placeShip(ship, 0, 0, "y");
   expect(gameboard.receiveAttack(0, 0)).toBe(true);
   expect(gameboard.receiveAttack(5, 5)).toBe(false);
 });
@@ -29,7 +29,7 @@ test("attack simulation - check coordinates and reports git/not hit", () => {
 test("updates hits value if ship was hit", () => {
   const gameboard = new Gameboard();
   const ship = new Ship(4);
-  gameboard.placeShip(ship, 0, 0, "vertically");
+  gameboard.placeShip(ship, 0, 0, "y");
   gameboard.receiveAttack(0, 0);
   expect(ship.hits).toBe(1);
 });
@@ -37,7 +37,7 @@ test("updates hits value if ship was hit", () => {
 test("update is sunk if ship was sunk", () => {
   const gameboard = new Gameboard();
   const ship = new Ship(1);
-  gameboard.placeShip(ship, 0, 0, "vertically");
+  gameboard.placeShip(ship, 0, 0, "y");
   gameboard.receiveAttack(0, 0);
   expect(ship.isSunk()).toBe(true);
 });
@@ -45,7 +45,7 @@ test("update is sunk if ship was sunk", () => {
 test("places 'miss' on empty field", () => {
   const gameboard = new Gameboard();
   const ship = new Ship(4);
-  gameboard.placeShip(ship, 0, 0, "vertically");
+  gameboard.placeShip(ship, 0, 0, "y");
   gameboard.receiveAttack(5, 5);
   expect(gameboard.board[5][5]).toBe("miss");
 });
@@ -54,7 +54,7 @@ test("checks if all ships are not sunk", () => {
   const gameboard = new Gameboard();
   const ship = new Ship(1);
   gameboard.ships.push(ship);
-  gameboard.placeShip(ship, 0, 0, "vertically");
+  gameboard.placeShip(ship, 0, 0, "y");
   expect(gameboard.areAllShipsSunk()).toBe(false);
 });
 
@@ -62,7 +62,7 @@ test.skip("checks if all ships are sunk after hitting last ship", () => {
   const gameboard = new Gameboard();
   const ship = new Ship(1);
   gameboard.ships.push(ship);
-  gameboard.placeShip(ship, 0, 0, "vertically");
+  gameboard.placeShip(ship, 0, 0, "y");
   gameboard.receiveAttack(0, 0);
   expect(gameboard.areAllShipsSunk()).toBe(true);
 });
