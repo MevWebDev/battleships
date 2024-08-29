d46749579c0696e60a8a179adc732ff4b3aeaeec
MevWebDev <szymongrysiewicz@gmail.com>
2024-08-29 23:13:25 +0200

    imports

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,16 @@
 import boardUI from "./boardUI";
+import header from "./header";
 import missBoardUI from "./missBoardUI";
+import popupComponent from "./popupComponent";
+
 export default function appUI() {
   const app = document.createElement("div");
   app.classList.add("app");
   document.body.appendChild(app);
+  document.body.appendChild(popupComponent());
   const boardsContainer = document.createElement("div");
   boardsContainer.classList.add("board-container");
+  app.appendChild(header());
   app.appendChild(boardsContainer);
   boardsContainer.appendChild(boardUI());
   boardsContainer.appendChild(missBoardUI());
