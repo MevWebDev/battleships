02e181fb67d5cccc2ee11b753b6e3a8b93f1d4ef
MevWebDev <szymongrysiewicz@gmail.com>
2024-09-11 14:46:03 +0200

    <feat> Function that shows player ships

---
+++ src/logic/displayPlayerShips.js
@@ -0,0 +1,17 @@
+export default function displayPlayerShips(player, board) {
+  const playerBoard = player.gameboard.board;
+
+  playerBoard.forEach((row, i) => {
+    row.forEach((field, idx) => {
+      const fieldDiv = board.querySelector(`.field-${i}-${idx}`);
+      if (fieldDiv) {
+        if (typeof field === "object" && field !== null) {
+          fieldDiv.classList.add("ship");
+          fieldDiv.style.backgroundColor = field.color;
+        }
+        if (field === "x" && field !== null)
+          fieldDiv.style.backgroundColor = "blue";
+      }
+    });
+  });
+}
