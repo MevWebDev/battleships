1fa3cd1e969d0ce28108c3dd1c95eaf1055d1189
MevWebDev <szymongrysiewicz@gmail.com>
2024-09-11 14:47:04 +0200

    <feat> Function that lets player attack enemy ships

---
+++ src/logic/playerAttacks.js
@@ -0,0 +1,26 @@
+import { computer } from "../index";
+import displayEnemyShips from "./displayEnemyShips";
+
+export default function playerAttack() {
+  const computerBoard = document.querySelector(".miss-board");
+
+  const fields = computerBoard.childNodes;
+  const computerShips = computer.gameboard;
+
+  fields.forEach((field) => {
+    field.addEventListener("click", () => {
+      const { className } = field;
+      const parts = className.split("-");
+      const x = parseInt(parts[1], 10);
+      const y = parseInt(parts[2].split(" ")[0], 10);
+
+      if (computerShips.receiveAttack(x, y) === true) {
+        field.classList.add("hit");
+        if (computerShips.areAllShipsSunk()) {
+          alert("You won!");
+        }
+      } else field.classList.add("miss");
+      displayEnemyShips(computer, computerBoard);
+    });
+  });
+}
