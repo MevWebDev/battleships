742c999fe04290ce5a50ee0b8d6e74c81f2632d2
MevWebDev <szymongrysiewicz@gmail.com>
2024-09-11 14:43:49 +0200

    <fix> Removed uneccesary imports

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
 import "./style.scss";
-import Ship from "./logic/ship";
-import Gameboard from "./logic/gameboard";
+
 import Player from "./logic/Player";
 import Computer from "./logic/computer";
 import appUI from "./components/app";
 import playerPutsShips from "./logic/playerPutsShips";
 import computerPutsShips from "./logic/computerPutsShips";
-import popupComponent from "./components/popupComponent";
+
 function startGame() {
   const player = new Player();
   const computer = new Computer();
   return { player, computer };
 }
 const { player, computer } = startGame();
-
+export { player, computer };
 appUI();
 playerPutsShips(player);
 computerPutsShips(computer);
-export { player, computer };
