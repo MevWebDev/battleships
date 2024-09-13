74a0169928ad8bd8f81c372d6bd80c389c737941
MevWebDev <szymongrysiewicz@gmail.com>
2024-09-13 23:54:58 +0200

    Initial dist subtree commit

--- src/components/header.js
+++
@@ -1,6 +0,0 @@
-export default function header() {
-  const headerDiv = document.createElement("div");
-  headerDiv.className = "header";
-  headerDiv.innerHTML = "<h1>Battleships</h1>";
-  return headerDiv;
-}
--- src/logic/result.js
+++
@@ -1,7 +0,0 @@
-import resultPopup from "../components/resultPopup";
-
-export default function (message, player, computer) {
-  player.reset();
-  computer.reset();
-  resultPopup(message);
-}
